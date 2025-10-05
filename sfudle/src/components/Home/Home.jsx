66d5f21c1e55f0855c43451d614240aa41b9a111
bdcruz727 <bryandelacz@gmail.com>
2025-10-04 21:13:28 -0700

    Basic home Page

--- sfudle/src/components/Home/Home.jsx
+++ sfudle/src/components/Home/Home.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react'
 import sfuLogo from '../../assets/sfuLogo.png'
+import mascot from '../../assets/mascot.png'
 
 function Home() {
 
     return(
         <div className='flex flex-col items-center justify-start min-h-screen'>
 
-            <div className='flex flex-row items-center'>
+            <div className='flex flex-row items-end w-80 mt-16'>
                 <img src={sfuLogo}>
                 
             </img>
+            <div className='flex flex-col'>
+                <img src={mascot}>
+                </img>
                 <h1 className='ml-2 font-bold text-3xl'>
-                DLE
+                    DLE
                 </h1>
             </div>
+                
+            </div>
             
-            <div>
-                <h1>
-                   Welcome
-                </h1>
+            <div className='flex flex-row items-center py-32 gap-16'>
+                <button className='menu-button'>
+                    <h2 className='text-[#fff9fc]'>
+                        Standard
+                    </h2>
+                </button>
+
+                <button className='menu-button'>
+                    <h2>
+                        Course
+                    </h2>
+                </button>
+
+                <button className='menu-button'>
+                    <h2>
+                        Location
+                    </h2>
+                </button>
             </div>
             
         </div>

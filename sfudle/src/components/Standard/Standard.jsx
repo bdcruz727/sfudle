3bccf73c6d97b36bec76e56f954b63e1eea00f67
bdcruz727 <bryandelacz@gmail.com>
2025-10-05 00:07:54 -0700

    Updated Home

--- sfudle/src/components/Standard/Standard.jsx
+++ sfudle/src/components/Standard/Standard.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import sfuLogo from '../../assets/sfuLogo.png'
 import mascot from '../../assets/mascot.png'
+import standardVictoryImage from '../../assets/standardVictoryImage.png'
 
 function Standard() {
     return(
-        <div>
-            <h1>
-                Standard
-            </h1>
-        </div>
+    
+            
+             <div 
+                className="min-h-screen flex flex-col items-center justify-center text-white w-full"
+                style={{
+                    backgroundImage: `url(${standardVictoryImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat'
+                }}
+            >
+                <h1>
+                    Standardafjsakjfhkjaeglkasjgkaelkgjklaejglkejklgjesklgjkesjglkjeskgjleksgjeslkjjklsjfkldsjfkdsjflnsdnfkldsjfldsj
+                </h1>
+            </div>
+
+
+       
 
     )
 }

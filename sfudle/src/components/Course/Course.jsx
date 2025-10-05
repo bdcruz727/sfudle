3841875965203b9367a9dcd90b7d7eefbd7a16cc
bdcruz727 <bryandelacz@gmail.com>
2025-10-05 09:27:48 -0700

    Push to main

--- sfudle/src/components/Course/Course.jsx
+++ sfudle/src/components/Course/Course.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import sfuLogo from '../../assets/sfuLogo.png'
-import mascot from '../../assets/mascot.png'
+import { useState, useEffect } from "react";
+import { getDetailsGuess, getRandomCourseDetails, getColours } from '../common/getCourseDetails.js';
+import sfuLogo from "../../assets/sfuLogo.png";
+import mascot from "../../assets/mascot.png";
+import { Link }  from 'react-router-dom'
+import allCourses from '../../data/courses.json'
+import standardVictory from '../../assets/standardVictoryImage.png'
 
 function Course() {
     return(
